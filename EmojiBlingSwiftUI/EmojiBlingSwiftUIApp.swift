005becc91d69bc0758d23c727059f8b147ad272c
Chu Yong <chuyong155@hellobike.com>
2023-08-03 09:59:28 +0800

    Initial Commit

---
+++ EmojiBlingSwiftUI/EmojiBlingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiBlingSwiftUIApp.swift
+//  EmojiBlingSwiftUI
+//
+//  Created by Chu Yong on 2023/8/3.
+//
+
+import SwiftUI
+
+@main
+struct EmojiBlingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
